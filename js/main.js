fd8b2f1e236bb5f43743d240ef4e8567a2ca47e4
Simon Welker <simon.welker@esirion.de>
2014-07-25 11:00:47 +0200

    JS: Add discardable proof-of-concept functionality to change tracklist, album art and title on drag&drop of files. CSS: Add overflow:hidden to prevent rubberband/bounce/whatever it's called, on OS X

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var remote = require('remote');
 var win = remote.getCurrentWindow();
+var fs = require('fs');
 
 // Global key event listener, we will trigger things like playback control through media keys from here
 window.onkeyup = function(e) {
@@ -15,18 +16,38 @@ window.ondragover = function(e) { e.preventDefault(); return false };
 window.ondrop = function(e) { e.preventDefault(); return false };
 
 // Global listener to drag&drop events, we will trigger library imports from here
-document.addEventListener('drop', function(e) {
+window.addEventListener('drop', function(e) {
 	e.preventDefault();
+
+	var cover = $('#main-content .cover img');
+	var tracklist = $('#main-content .tracklist');
+	var header = $('#main-header-content');
+
+	tracklist.html(''); // clear track list
 
 	// Get the list of files that were dropped on the application
 	var droppedFiles = e.dataTransfer.files;
 	for(var i = 0; i < droppedFiles.length; i++) {
-		// do something with each file
+		(function(i) {
+			var asset = AV.Asset.fromFile(droppedFiles[i]);
+
+			asset.get('metadata', function(data) {
+				var tracklistEntry = $('<div class="track"><div><span class="tracknumber">' + data.tracknumber
+					+ '</span>' + data.title + '</div></div>');
+				tracklist.append(tracklistEntry);
+
+			  if(i === 0) {
+			  	var headerContent = $('<span class="part">' + data.artist + '</span><span class="part">' + data.album + '</span>');
+			  	header.html(headerContent);
+
+			  	cover.attr('src', data.coverArt.toBlobURL()); // set cover to cover art of first file in list
+			  }
+			});
+		})(i);
 	}
 
 	return false;
 });
-
 
 // --- should be extracted
 document.querySelector('#close-win').addEventListener('click', function(e) {
@@ -49,7 +70,5 @@ window.addEventListener('polymer-ready', function() {
   var drawer = document.getElementById('outer-drawer');
   drawerButton.addEventListener('click', function() {
     drawer.togglePanel();
-  });
-
-  
+  });  
 });
