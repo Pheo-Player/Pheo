794c19c09b7efd4bfa09c65ad2d1a9ddb11c3c43
Simon Welker <simon.welker@outlook.com>
2014-08-13 21:04:04 +0200

    Remove some code that we won't need. Make the server safer from directory traversal vulnerability and use the error case properly.

--- client/app/audio-asset/audio-asset.service.js
+++ client/app/audio-asset/audio-asset.service.js
@@ -18,44 +18,4 @@ angular.module('pheoApp')
 
 			return deferred.promise;
 		};
-
-		/*var mmm = require('mmmagic');
-		var magic = new mmm.Magic();
-		var ALLOWED_AUDIO_FORMATS = [
-			'audio/basic',    // uLaw
-			'audio/L24',      // LPCM
-			'audio/mpeg',     // mp3
-			'audio/ogg',      // ogg vorbis
-			'audio/vorbis',   // vorbis
-			'audio/vnd.wave', // WAV
-			'audio/wav',      // WAV
-			'audio/flac',     // FLAC
-			'audio/alac'      // ALAC
-		];*/
-		/**
-		 * Checks if a given buffer is in an audio format that can be decoded.
-		 * @param {Buffer} buffer The data buffer to be checked
-		 * @return {promise} A promise to fail with an exception, or resolve with the given buffer if data is decodable audio
-		 */
-		function checkType(buffer) {
-			var deferred = $q.defer();
-
-			try {
-				// TODO actual checks as soon as mmmagic works
-				
-				// magic.detect(buf, function(err, result) {
-				// 	if(err) throw err;
-
-				// 	if(...) {
-				// 		deferred.resolve(buf);
-				// 	}
-				// });
-				
-				deferred.resolve(buffer);
-			} catch(e) {
-				deferred.reject(e);
-			}
-
-			return deferred.promise;
-		}
 	}]);
