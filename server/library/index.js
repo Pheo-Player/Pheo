2e9f9b3b6ee4e8692155f4624fe35976667a06d6
Simon Welker <simon.welker@esirion.de>
2014-08-15 14:20:06 +0200

    Add READ action to library routes and comment the whole file

--- server/library/index.js
+++ server/library/index.js
@@ -3,24 +3,43 @@ var express = require('express'),
 
     Library = require('./library');
 
+// Create and initialise the library
 var library = new Library();
 library.init();
 
-app.get('/library', function(req, res) {
-	if(library.store) {
-		library.store.find({}, function(err, data) {
-			if(err) {
-				res.status(500);
-				res.end('Error while reading library');
-			}
+// We will reuse this function any time accessing the library fails
+function endWithLibraryError(res) {
+	res.status(500);
+	res.end('Error while reading library');
+}
 
-			res.json(data);
-		});
-	}
-	else {
+// Middleware to check if the library is ready for all requests
+app.use(function(req, res, next) {
+	if(!library.store) {
 		res.status(404);
 		res.end('Library is not ready');
 	}
+	else {
+		next();
+	}
+});
+
+// INDEX action
+app.get('/library', function(req, res) {
+	library.store.find({}, function(err, data) {
+		if(err) endWithLibraryError(res);
+
+		res.json(data);
+	});
+});
+
+// READ action
+app.get('/library/:id', function(req, res) {
+	library.store.findOne({ _id: req.params.id }, function(err, data) {
+		if(err) endWithLibraryError(res);
+
+		res.json(data);
+	});
 });
 
 module.exports = app;
