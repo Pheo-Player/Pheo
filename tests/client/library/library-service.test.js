1c00f2a8d5749e2138cfda73f3653cf52bde678a
Simon Welker <simon.welker@esirion.de>
2014-08-20 13:31:31 +0200

    Remove obsolete/unnecessary attributes from test data

--- tests/client/library/library-service.test.js
+++ tests/client/library/library-service.test.js
@@ -8,16 +8,16 @@ describe('Library Service', function() {
 	var httpMock;
 
 	// Mock values and objects
-	var a0t1 = {"file":"/path/lo.mp3","mtime":"2014-08-01T00:00:00.000Z","_id":"Shimeldimeldidum","metadata":{"title":"Lo","artist":["Bobby and the Tests"],"albumartist":[],"album":"Hi / Lo","year":"2014","track":{"no":1,"of":0},"genre":["Retrofunk"],"disk":{"no":0,"of":0},"duration":0}},
-	a0t2 = {"file":"/path/hi.mp3","mtime":"2014-08-01T00:00:00.000Z","_id":"ShaKar0nSh4kArOn","metadata":{"title":"Hi","artist":["Bobby and the Tests"],"albumartist":[],"album":"Hi / Lo","year":"2014","track":{"no":2,"of":0},"genre":["Neue Dauer Welle"],"disk":{"no":0,"of":0},"duration":0}},
+	var a0t1 = {"_id":"Shimeldimeldidum","metadata":{"title":"Lo","artist":["Bobby and the Tests"],"albumartist":[],"album":"Hi / Lo","year":"2014","track":{"no":1,"of":0},"genre":["Retrofunk"],"disk":{"no":0,"of":0},"duration":0}},
+	a0t2 = {"_id":"ShaKar0nSh4kArOn","metadata":{"title":"Hi","artist":["Bobby and the Tests"],"albumartist":[],"album":"Hi / Lo","year":"2014","track":{"no":2,"of":0},"genre":["Neue Dauer Welle"],"disk":{"no":0,"of":0},"duration":0}},
 
-	a1t1 = {"file":"/path/noalbum.mp3","mtime":"2014-06-20T00:00:00.000Z","_id":"ThisHasNoAlbumYo","metadata":{"title":"Track without an album","artist":["Dawg"],"albumartist":[""],"album":"","year":"1979","track":{"no":0,"of":0},"genre":[""],"disk":{"no":0,"of":0},"duration":0}},
+	a1t1 = {"_id":"ThisHasNoAlbumYo","metadata":{"title":"Track without an album","artist":["Dawg"],"albumartist":[""],"album":"","year":"1979","track":{"no":0,"of":0},"genre":[""],"disk":{"no":0,"of":0},"duration":0}},
 
-	a2t1 = {"file":"/path/notracknoa.mp3","mtime":"2014-06-20T00:00:00.000Z","_id":"ThisHasNoTrackNo","metadata":{"title":"Track without a number","artist":["Dawg"],"albumartist":[""],"album":"Unsorted Album","year":"1979","track":{"no":0,"of":0},"genre":[""],"disk":{"no":0,"of":0},"duration":0}},
-	a2t2 = {"file":"/path/notracknob.mp3","mtime":"2014-06-20T00:00:00.000Z","_id":"ThatHasNoTrackNo", "metadata":{"title":"Song without a number","artist":["Dawg"],"albumartist":[""],"album":"Unsorted Album","year":"1979","track":{"no":0,"of":0},"genre":[""],"disk":{"no":0,"of":0},"duration":0}},
+	a2t1 = {"_id":"ThisHasNoTrackNo","metadata":{"title":"Track without a number","artist":["Dawg"],"albumartist":[""],"album":"Unsorted Album","year":"1979","track":{"no":0,"of":0},"genre":[""],"disk":{"no":0,"of":0},"duration":0}},
+	a2t2 = {"_id":"ThatHasNoTrackNo", "metadata":{"title":"Song without a number","artist":["Dawg"],"albumartist":[""],"album":"Unsorted Album","year":"1979","track":{"no":0,"of":0},"genre":[""],"disk":{"no":0,"of":0},"duration":0}},
 
-	a3t1 = {"file":"/path/compilationa.mp3","mtime":"2014-07-01T00:00:00.000Z","_id":"DieWutzIstFettJa","metadata":{"title":"Before It Was Cool","artist":["The Books"],"albumartist":["Various Artists"],"album":"Nase","year":"2011","track":{"no":1,"of":2},"genre":["Some Hipster shit"],"disk":{"no":0,"of":0},"duration":0}},
-	a3t2 = {"file":"/path/compilationb.mp3","mtime":"2014-06-20T00:00:00.000Z","_id":"OstfriesenGold1a","metadata":{"title":"Ostfriesengold","artist":["Someone"],"albumartist":["Various Artists"],"album":"Nase","year":"2011","track":{"no":2,"of":2},"genre":["Some Hipster shit"],"disk":{"no":0,"of":0},"duration":0}};
+	a3t1 = {"_id":"DieWutzIstFettJa","metadata":{"title":"Before It Was Cool","artist":["The Books"],"albumartist":["Various Artists"],"album":"Nase","year":"2011","track":{"no":1,"of":2},"genre":["Some Hipster shit"],"disk":{"no":0,"of":0},"duration":0}},
+	a3t2 = {"_id":"OstfriesenGold1a","metadata":{"title":"Ostfriesengold","artist":["Someone"],"albumartist":["Various Artists"],"album":"Nase","year":"2011","track":{"no":2,"of":2},"genre":["Some Hipster shit"],"disk":{"no":0,"of":0},"duration":0}};
 	var mockLibrary = _.shuffle([ // random initial sorting
 		a0t1, a0t2, a1t1, a2t1, a2t2, a3t1, a3t2
 	]);
